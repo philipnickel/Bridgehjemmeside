7896484ad847530e550eaf5f6a7f78ec76917d3b
philipnickel <tipkik@hotmail.com>
2024-03-14 22:25:38 +0100

    new

--- app.py
+++ app.py
@@ -3,12 +3,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
 
 @app.route('/login')
 def login():
     return render_template('login.html')
+
+@app.route('/index')
+def index2():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
