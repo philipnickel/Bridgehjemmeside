12e48f7d181df5d4bc3923d42a4af5f5243a84c2
philipnickel <tipkik@hotmail.com>
2024-03-23 00:01:27 +0100

    hm

--- bridge_club_management/bridge_club_management/settings.py
+++ bridge_club_management/bridge_club_management/settings.py
@@ -125,7 +125,7 @@ LOCALE_PATHS = [
 
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -146,4 +146,4 @@ CKEDITOR_CONFIGS = {
 }
 
 
-STATIC_ROOT = '/static'
+STATIC_ROOT = '/home/Philipnickel/Bridgehjemmeside/bridge_club_management/static'
