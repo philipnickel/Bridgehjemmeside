d7a7c6ef55d004a212dc5343341bc09e83d8a7cd
philipnickel <tipkik@hotmail.com>
2024-09-01 20:03:19 +0200

    fix attempt

--- bridge_club_management/club_management/apps.py
+++ bridge_club_management/club_management/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
-
+import os
 
 class ClubManagementConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'club_management'
+    path = os.path.dirname(os.path.abspath(__file__))
