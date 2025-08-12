e3991cf9ca91f349067bf7248b1bd7a2fca68872
philipnickel <tipkik@hotmail.com>
2025-08-12 14:44:45 +0200

    staging: accept DJANGO_EMAIL_HOST_USER/PASSWORD env vars; ensure test-site can reuse production-style credentials

--- bridge_club_management/bridge_club_management/settings/staging.py
+++ bridge_club_management/bridge_club_management/settings/staging.py
@@ -84,8 +84,9 @@ if ENABLE_EMAILS or EMAIL_BACKEND.endswith('smtp.EmailBackend'):
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.gmail.com')
     EMAIL_PORT = int(os.environ.get('EMAIL_PORT', '587'))
-    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
-    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
+    # Accept both EMAIL_HOST_USER and DJANGO_EMAIL_HOST_USER
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER') or os.environ.get('DJANGO_EMAIL_HOST_USER', '')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD') or os.environ.get('DJANGO_EMAIL_HOST_PASSWORD', '')
     EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'true').lower() == 'true'
 
 # Development toolbar for staging
